84232cd8da1fac70079a7eae1565235a37575140
Alex Turpin <turpinalex825@gmail.com>
2023-02-05 01:22:31 -0500

    Implement number generator

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 /*
-	Random number generator 
+	Random-Number-Generator.cpp
 
-	randomly generate a number between 1-100
+	randomly generates 9 numbers between 1-100
 */
 
 #include <iostream>
+#include <cstdlib>
+
+int random(int num)
+{
+	num = rand() % 100;
+	return num;
+}
 
 int main()
 {
+	srand(time(0));
 	int number = 0;
-
-	std::cout << "Hello my name is Alex" << std::endl;
+	int output = 0;
+	for (int i = 0; i < 9; i++) {
+		output = random(number);
+		std::cout << output << std::endl;
+	}
 
 	return 0;
 }
